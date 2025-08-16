9b5bd10af9b5b3838ba55b6f621e4ea6e35d56d1
XVVQAQ <XVVQAQ143352@outlook.com>
2025-08-17 04:36:17 +0800

    Initialize

---
+++ nuxt.config.ts
@@ -0,0 +1,21 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+import Components from 'unplugin-vue-components/vite'
+import RekaResolver from 'reka-ui/resolver'
+
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+  css: ['~/assets/main.css'],
+  vite:{
+    plugins:[
+      tailwindcss(),
+      Components({
+        dts: true,
+        resolvers: [
+          RekaResolver()
+        ],
+      }),
+    ]
+  }
+})
